ea694781e1a439e68d86c70315d863bf1d3aeb9d
miratava <miratava@gmail.com>
2020-09-26 15:48:27 +0200

    Added creation directory

--- browser.py
+++ browser.py
@@ -5,7 +5,7 @@ import enum
 nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
 
-Scientists have created “soft” magnets that can flow 
+Scientists have created "soft" magnets that can flow 
 and change shape, and that could be a boon to medicine 
 and robotics. (Source: New York Times)
 
@@ -33,7 +33,7 @@ It's 50 years since the world was gripped by historic images
 Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
 
 Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
+ addressed Apple Inc. employees at the iPhone maker's headquarters
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 i'''
 
@@ -47,7 +47,7 @@ class Page():
         return self.url
 
     def get_text(self):
-        return  self.text
+        return self.text
 
 
 class Status(enum.Enum):
@@ -58,35 +58,31 @@ class Status(enum.Enum):
 class Browser():
     error_message = "Error"
 
-    def is_validate_input(self, str_input):
-        dot = "."
-        if dot in str_input:
-            return True
-        return False
-
-    def is_page_exist(self, str_input, page: Page):
-        if str_input == page.get_url():
-            return True
-        return False
-
-    def load_page(self, str_input, directory, page: Page):
-        if self.is_validate_input(str_input):
-            self.create_file(str_input, directory, page)
-            if self.is_page_exist(str_input, page):
-                return Status.ok, page.get_text()
-        return Status.error, self.error_message
+    def load_page(self, str_input, directory, page):
+        self.create_file(str_input, directory, page)
+        return Status.ok, page.get_text()
 
     @staticmethod
-    def create_file(str_input, directory, page: Page):
-        dot = "."
-        try:
-            os.mkdir(directory)
-        except FileExistsError:
+    def create_directory(directory):
+        parent_dir = os.getcwd()
+        path = os.path.join(parent_dir, directory)
+        if os.path.isdir(path):
             pass
-        file_name = page.get_url().split(dot)[0]
-        if not os.path.exists(directory + "\\" + file_name):
-            with open(file_name, "w") as f:
-                f.write(page.get_text())
+        else:
+            os.mkdir(path)
+        return path
+
+    @staticmethod
+    def create_file(str_input, directory, page):
+        path = self.create_directory(directory)
+        dot = "."
+        file_name = os.path.join(path, page.get_url().split(dot)[0])
+       # if not os.path.isfile(file_name):
+        with open(file_name, "w") as f:
+            f.write(page.get_text())
+
+    def get_error_message(self):
+        return self.error_message
 
 
 def main():
@@ -94,18 +90,21 @@ def main():
     browser = Browser()
     args = sys.argv
     directory = args[-1]
-    input_str = ""
+    browser.create_directory(directory)
+    str_input = ""
     exit_from_program = "exit"
-    while input_str != exit_from_program:
-        input_str = input()
-        for item in urls.items():
-            page = lambda x, y : Page(item[0], item[1])
-            status, value = browser.load_page(url, directory, page)
+    while str_input != exit_from_program:
+        str_input = input()
+        if str_input in urls:
+            page = Page(str_input, urls.get(str_input))
+            status, value = browser.load_page(str_input, directory, page)
             if value is not None:
                 print(value)
             if status is Status.error:
                 continue
+        else:
+            print(browser.error_message)
+            continue
 
 
 main()
->>>>>>> Added classes
